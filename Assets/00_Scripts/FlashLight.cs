418490be0583b5c010b9ad682314539f4699fa5b
WhiteMango <shinwltjd04@naver.com>
2025-10-20 21:05:38 +0900

    Player_Interact
    플레이어의 카메라 레이캐스트를 이용해서
    인터렉션을 구현함.
    
    손전등 기능 구현 완료

---
+++ Assets/00_Scripts/FlashLight.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject NormalFlashLight;
+    public GameObject LongRangeFlashLight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (NormalFlashLight.activeSelf || LongRangeFlashLight.activeSelf)
+            {
+                NormalFlashLight.SetActive(false);
+                LongRangeFlashLight.SetActive(false);
+            }
+            else
+            {
+                NormalFlashLight.SetActive(true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (NormalFlashLight.activeSelf || LongRangeFlashLight.activeSelf)
+            {
+                if (LongRangeFlashLight != null)
+                {
+                    if (NormalFlashLight.activeSelf)
+                    {
+                        NormalFlashLight.SetActive(false);
+                        LongRangeFlashLight.SetActive(true);
+                    }
+                    else if (LongRangeFlashLight.activeSelf)
+                    {
+                        LongRangeFlashLight.SetActive(false);
+                        NormalFlashLight.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
